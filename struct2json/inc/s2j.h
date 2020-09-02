ff7e00b1f0a495db2904c85d870bb4794d73750d
linning <lin.ning@hotmail.com>
2020-09-02 06:01:53 +0000

    check NULL pointer before parsing CJSON object

--- struct2json/inc/s2j.h
+++ struct2json/inc/s2j.h
@@ -38,7 +38,7 @@ extern "C" {
 #endif
 
 /* struct2json software version number */
-#define S2J_SW_VERSION                "1.0.4"
+#define S2J_SW_VERSION                "1.0.5"
 
 /* Create JSON object */
 #define s2j_create_json_obj(json_obj) \
@@ -52,13 +52,67 @@ extern "C" {
 #define s2j_json_set_basic_element(to_json, from_struct, type, element) \
     S2J_JSON_SET_BASIC_ELEMENT(to_json, from_struct, type, element)
 
+#define s2j_json_set_basic_element_isused(to_json, from_struct, type, element) \
+    { \
+		if (from_struct->element ## _isUsed) { \
+			S2J_JSON_SET_BASIC_ELEMENT(to_json, from_struct, type, element) \
+		} \
+	}
+
+#define s2j_json_set_basic_element_withlen(to_json, from_struct, type, element) \
+    { \
+		if (from_struct->element ## Len > 0) { \
+			S2J_JSON_SET_BASIC_ELEMENT(to_json, from_struct, type, element) \
+		} \
+	}
+
+#define s2j_json_set_array_element_withlen(to_json, from_struct, type, element) \
+    { \
+		if (from_struct->element ## Len > 0) { \
+			S2J_JSON_SET_ARRAY_ELEMENT(to_json, from_struct, type, element, from_struct->element ## Len) \
+		} \
+	}
+
 /* Set array type element for JSON object */
 #define s2j_json_set_array_element(to_json, from_struct, type, element, size) \
     S2J_JSON_SET_ARRAY_ELEMENT(to_json, from_struct, type, element, size)
 
 /* Set child structure type element for JSON object */
-#define s2j_json_set_struct_element(child_json, to_json, child_struct, from_struct, type, element) \
+#define s2j_json_set_struct_element_start(child_json, to_json, child_struct, from_struct, type, element) \
     S2J_JSON_SET_STRUCT_ELEMENT(child_json, to_json, child_struct, from_struct, type, element)
+
+#define s2j_json_set_struct_element_end(child_json, to_json, child_struct, from_struct, type, element)    
+
+#define s2j_json_set_struct_element_isused_start(child_json, to_json, child_struct, from_struct, type, element) \
+    { \
+		if (from_struct->element ## _isUsed) { \
+    		S2J_JSON_SET_STRUCT_ELEMENT(child_json, to_json, child_struct, from_struct, type, element)
+
+#define s2j_json_set_struct_element_isused_end(child_json, to_json, child_struct, from_struct, type, element) \
+		} \
+	}
+
+/* Set child structure type element for structure object */
+#define s2j_struct_set_struct_element(child_struct, to_struct, child_json, from_json, type, element) \
+    S2J_STRUCT_SET_STRUCT_ELEMENT(child_struct, to_struct, child_json, from_json, type, element)
+
+/* Set enum type element for struct object */
+#define s2j_json_set_enum_element(to_json, from_struct, element, enum_string) \
+    S2J_JSON_SET_ENUM_ELEMENT(to_json, from_struct, element, enum_string)
+
+#define s2j_json_set_enum_element_isused(to_json, from_struct, element, enum_string) \
+    { \
+		if (from_struct->element ## _isUsed) {\
+    		S2J_JSON_SET_ENUM_ELEMENT(to_json, from_struct, element, enum_string) \
+		}\
+	}
+
+/* Set child struct array element from struct array object*/
+#define s2j_json_set_structArray_element_start(array_json, to_json, item_json, from_struct, array_struct, type, _element, _level) \
+    S2J_JSON_SET_STRUCTARRAY_ELEMENT_START(array_json, to_json, item_json, from_struct, array_struct, type, _element, _level)
+
+#define s2j_json_set_structArray_element_end(array_json, to_json, array_struct, item_json, element, _level) \
+    S2J_JSON_SET_STRUCTARRAY_ELEMENT_END(array_json, to_json, array_struct, item_json, element, _level)
 
 /* Create structure object */
 #define s2j_create_struct_obj(struct_obj, type) \
@@ -71,19 +125,42 @@ extern "C" {
 /* Get basic type element for structure object */
 #define s2j_struct_get_basic_element(to_struct, from_json, type, element) \
     S2J_STRUCT_GET_BASIC_ELEMENT(to_struct, from_json, type, element)
-#define s2j_struct_get_basic_element_ex(to_struct, from_json, type, element, _defval) \
-    S2J_STRUCT_GET_BASIC_ELEMENT_EX(to_struct, from_json, type, element, _defval)
+
+#define s2j_struct_get_basic_element_isused(to_struct, from_json, type, element) \
+    S2J_STRUCT_GET_BASIC_ELEMENT_ISUSED(to_struct, from_json, type, element)
 
 /* Get array type element for structure object */
-#define s2j_struct_get_array_element(to_struct, from_json, type, element) \
-    S2J_STRUCT_GET_ARRAY_ELEMENT(to_struct, from_json, type, element)
-
-#define s2j_struct_get_array_element_ex(to_struct, from_json, type, element, size, _defval) \
-    S2J_STRUCT_GET_ARRAY_ELEMENT_EX(to_struct, from_json, type, element, size, _defval)
+#define s2j_struct_get_array_element(to_struct, from_json, type, element, len) \
+    S2J_STRUCT_GET_ARRAY_ELEMENT(to_struct, from_json, type, element, len)
 
 /* Get child structure type element for structure object */
 #define s2j_struct_get_struct_element(child_struct, to_struct, child_json, from_json, type, element) \
     S2J_STRUCT_GET_STRUCT_ELEMENT(child_struct, to_struct, child_json, from_json, type, element)
+
+#define s2j_struct_get_struct_element_start(child_struct, to_struct, child_json, from_json, type, element) \
+    S2J_STRUCT_GET_STRUCT_ELEMENT(child_struct, to_struct, child_json, from_json, type, element)
+
+#define s2j_struct_get_struct_element_end(child_struct, to_struct, child_json, from_json, type, element)   
+
+#define s2j_struct_get_struct_element_isused_start(child_struct, to_struct, child_json, from_json, type, element) \
+    S2J_STRUCT_GET_STRUCT_ELEMENT_ISUSED_START(child_struct, to_struct, child_json, from_json, type, element)
+
+#define s2j_struct_get_struct_element_isused_end(child_struct, to_struct, child_json, from_json, type, element) \
+    S2J_STRUCT_GET_STRUCT_ELEMENT_ISUSED_END(child_struct, to_struct, child_json, from_json, type, element)
+
+/* Get enum type element for struct object */
+#define s2j_struct_get_enum_element(to_struct, from_json, element, enum_string) \
+    S2J_STRUCT_GET_ENUM_ELEMENT(to_struct, from_json, element, enum_string)
+	
+#define s2j_struct_get_enum_element_isused(to_struct, from_json, element, enum_string) \
+    S2J_STRUCT_GET_ENUM_ELEMENT_ISUSED(to_struct, from_json, element, enum_string)
+
+/* Get child struct array element from struct array object*/
+#define s2j_struct_get_structArray_element_start(child_struct, to_struct, from_json, array_json, item_json, type, element, level) \
+    S2J_STRUCT_GET_STRUCTARRAY_ELEMENT_START(child_struct, to_struct, from_json, array_json, item_json,  type, element, level)
+
+#define s2j_struct_get_structArray_element_end(child_struct, to_struct, type, element) \
+    S2J_STRUCT_GET_STRUCTARRAY_ELEMENT_END(child_struct, to_struct, type, element)
 
 /* s2j.c */
 extern S2jHook s2jHook;
--- struct2json/inc/s2jdef.h
+++ struct2json/inc/s2jdef.h
@@ -41,55 +41,85 @@ typedef struct {
     void (*free_fn)(void *ptr);
 } S2jHook, *S2jHook_t;
 
-#define S2J_STRUCT_GET_int_ELEMENT(to_struct, from_json, _element) \
-    json_temp = cJSON_GetObjectItem(from_json, #_element); \
-    if (json_temp) (to_struct)->_element = json_temp->valueint;
+#define S2J_STRUCT_ISUSED_BASIC_ELEMENT(to_struct, from_json, _element) \
+    (to_struct)->_element ## _isUsed = 1;
 
-#define S2J_STRUCT_GET_int_ELEMENT_EX(to_struct, from_json, _element, _defval) \
-    { \
-        if (from_json) { \
-            json_temp = cJSON_GetObjectItem(from_json, #_element); \
-            if (json_temp) (to_struct)->_element = json_temp->valueint; \
-            else (to_struct)->_element = _defval; \
-        } else { \
-            (to_struct)->_element = _defval; \
+#define S2J_STRUCT_GET_ENUM_ELEMENT(to_struct, from_json, _element, enum_string) \
+    if (from_json) { \
+        json_temp = cJSON_GetObjectItem(from_json, #_element); \
+        if (json_temp) {\
+            for(int i=0; i< sizeof(enum_string)/sizeof(enum_string[0]); i++) { \
+                if((json_temp->valuestring) && (0 == strcmp(enum_string[i], json_temp->valuestring))) {\
+                    (to_struct)->_element = i;\
+                }\
+            }\
         } \
     }
+
+#define S2J_STRUCT_GET_ENUM_ELEMENT_ISUSED(to_struct, from_json, _element, enum_string) \
+    if (from_json) { \
+        json_temp = cJSON_GetObjectItem(from_json, #_element); \
+        if (json_temp) {\
+            for(int i=0; i< sizeof(enum_string)/sizeof(enum_string[0]); i++) { \
+                if((json_temp->valuestring) && (0 == strcmp(enum_string[i], json_temp->valuestring))) {\
+                    (to_struct)->_element = i;\
+                }\
+            }\
+    		(to_struct)->_element ## _isUsed = 1; \
+        } \
+    	else (to_struct)->_element ## _isUsed = 0; \
+    } else (to_struct)->_element ## _isUsed = 0;
+
+#define S2J_STRUCT_GET_int_ELEMENT(to_struct, from_json, _element) \
+    if (from_json) { \
+        json_temp = cJSON_GetObjectItem(from_json, #_element); \
+        if (json_temp) (to_struct)->_element = json_temp->valueint; \
+    }
+
+#define S2J_STRUCT_GET_int_ELEMENT_ISUSED(to_struct, from_json, _element) \
+    if (from_json) { \
+        json_temp = cJSON_GetObjectItem(from_json, #_element); \
+        if (json_temp) { (to_struct)->_element = json_temp->valueint; (to_struct)->_element ## _isUsed = 1;} \
+    	else (to_struct)->_element ## _isUsed = 0; \
+    } else (to_struct)->_element ## _isUsed = 0;
+
+#define S2J_STRUCT_GET_string_ELEMENT_ISUSED(to_struct, from_json, _element) \
+	S2J_STRUCT_GET_string_ELEMENT(to_struct, from_json, _element)
 
 #define S2J_STRUCT_GET_string_ELEMENT(to_struct, from_json, _element) \
-    json_temp = cJSON_GetObjectItem(from_json, #_element); \
-    if (json_temp) strcpy((to_struct)->_element, json_temp->valuestring);
-
-#define S2J_STRUCT_GET_string_ELEMENT_EX(to_struct, from_json, _element, _defval) \
-    { \
-        if (from_json) { \
-            json_temp = cJSON_GetObjectItem(from_json, #_element); \
-            if (json_temp) strcpy((to_struct)->_element, json_temp->valuestring); \
-            else strcpy((to_struct)->_element, _defval); \
-        } else { \
-            strcpy((to_struct)->_element, _defval); \
+    if (from_json) { \
+        json_temp = cJSON_GetObjectItem(from_json, #_element); \
+        if (json_temp) {\
+            if (json_temp->valuestring) \
+            { \
+                strcpy((to_struct)->_element, json_temp->valuestring); \
+                (to_struct)->_element ## Len = strlen(json_temp->valuestring);\
+            } \
+            else \
+            {(to_struct)->_element ## Len = 0; } \
         } \
-    }
+    	else \
+    		(to_struct)->_element ## Len = 0; \
+    } else (to_struct)->_element ## Len = 0; 
 
 #define S2J_STRUCT_GET_double_ELEMENT(to_struct, from_json, _element) \
-    json_temp = cJSON_GetObjectItem(from_json, #_element); \
-    if (json_temp) (to_struct)->_element = json_temp->valuedouble;
-
-#define S2J_STRUCT_GET_double_ELEMENT_EX(to_struct, from_json, _element, _defval) \
-    { \
-        if (from_json) { \
-            json_temp = cJSON_GetObjectItem(from_json, #_element); \
-            if (json_temp) (to_struct)->_element = json_temp->valuedouble; \
-            else (to_struct)->_element = _defval; \
-        } else { \
-            (to_struct)->_element = _defval; \
-        } \
+    if (from_json) {\
+        json_temp = cJSON_GetObjectItem(from_json, #_element); \
+        if (json_temp) (to_struct)->_element = json_temp->valuedouble; \
     }
+
+#define S2J_STRUCT_GET_double_ELEMENT_ISUSED(to_struct, from_json, _element) \
+    if (from_json) { \
+        json_temp = cJSON_GetObjectItem(from_json, #_element); \
+        if (json_temp) { (to_struct)->_element = json_temp->valuedouble; (to_struct)->_element ## _isUsed = 1; }\
+    	else	(to_struct)->_element ## _isUsed = 0; \
+    } else (to_struct)->_element ## _isUsed = 0;
 
 #define S2J_STRUCT_ARRAY_GET_int_ELEMENT(to_struct, from_json, _element, index) \
     (to_struct)->_element[index] = from_json->valueint;
 
 #define S2J_STRUCT_ARRAY_GET_string_ELEMENT(to_struct, from_json, _element, index) \
+    if (from_json && from_json->valuestring) \
     strcpy((to_struct)->_element[index], from_json->valuestring);
 
 #define S2J_STRUCT_ARRAY_GET_double_ELEMENT(to_struct, from_json, _element, index) \
@@ -98,20 +128,21 @@ typedef struct {
 #define S2J_STRUCT_ARRAY_GET_ELEMENT(to_struct, from_json, type, _element, index) \
     S2J_STRUCT_ARRAY_GET_##type##_ELEMENT(to_struct, from_json, _element, index)
 
-#define S2J_STRUCT_ARRAY_GET_int_ELEMENT_EX(to_struct, from_json, _element, index, _defval) \
+#define S2J_STRUCT_ARRAY_GET_int_STRUCT_ELEMENT(to_struct, from_json, _element, index, _level) \
     if (from_json) (to_struct)->_element[index] = from_json->valueint; \
-    else (to_struct)->_element[index] = _defval; 
-    
-#define S2J_STRUCT_ARRAY_GET_string_ELEMENT_EX(to_struct, from_json, _element, index, _defval) \
-    if (from_json) strcpy((to_struct)->_element[index], from_json->valuestring); \
-    else strcpy((to_struct)->_element[index], _defval); 
+    else (to_struct)->_element[index] = 0; 
 
-#define S2J_STRUCT_ARRAY_GET_double_ELEMENT_EX(to_struct, from_json, _element, index) \
+#define S2J_STRUCT_ARRAY_GET_string_STRUCT_ELEMENT(to_struct, from_json, _element, index, _level) \
+    if (from_json && from_json->valuestring) \
+    strcpy((to_struct)->_element[index], from_json->valuestring); \
+    else strcpy((to_struct)->_element[index], ""); 
+
+#define S2J_STRUCT_ARRAY_GET_double_STRUCT_ELEMENT(to_struct, from_json, _element, index, _level) \
     if (from_json) (to_struct)->_element[index] = from_json->valuedouble; \
-    else (to_struct)->_element[index] = _defval;
+    else (to_struct)->_element[index] = 0;
 
-#define S2J_STRUCT_ARRAY_GET_ELEMENT_EX(to_struct, from_json, type, _element, index, _defval) \
-    S2J_STRUCT_ARRAY_GET_##type##_ELEMENT_EX(to_struct, from_json, _element, index, _defval)
+#define S2J_STRUCT_ARRAY_GET_STRUCT_ELEMENT(to_struct, from_json, type, _element, index, _level) \
+    S2J_STRUCT_ARRAY_GET_##type##_STRUCT_ELEMENT(to_struct, from_json, _element, index, _level)
 
 #define S2J_JSON_SET_int_ELEMENT(to_json, from_struct, _element) \
     cJSON_AddNumberToObject(to_json, #_element, (from_struct)->_element);
@@ -121,6 +152,9 @@ typedef struct {
 
 #define S2J_JSON_SET_string_ELEMENT(to_json, from_struct, _element) \
     cJSON_AddStringToObject(to_json, #_element, (from_struct)->_element);
+
+#define S2J_JSON_SET_boolean_ELEMENT(to_json, from_struct, _element) \
+    cJSON_AddBoolToObject(to_json, #_element, (from_struct)->_element);
 
 #define S2J_JSON_ARRAY_SET_int_ELEMENT(to_json, from_struct, _element, index) \
     cJSON_AddItemToArray(to_json, cJSON_CreateNumber((from_struct)->_element[index]));
@@ -134,6 +168,19 @@ typedef struct {
 #define S2J_JSON_ARRAY_SET_ELEMENT(to_json, from_struct, type, _element, index) \
     S2J_JSON_ARRAY_SET_##type##_ELEMENT(to_json, from_struct, _element, index)
 
+//todo
+#define S2J_JSON_ARRAY_SET_int_STRUCT_ELEMENT(array_json, array_struct, element ) \
+	cJSON_AddNumberToObject(array_json, #element, array_struct->element)
+
+#define S2J_JSON_ARRAY_SET_string_STRUCT_ELEMENT(array_json, array_struct, element ) \
+	cJSON_AddStringToObject(array_json, #element, array_struct->element)
+
+#define S2J_JSON_ARRAY_SET_double_STRUCT_ELEMENT(array_json, array_struct, element ) \
+	cJSON_AddNumberToObject(array_json, #element, array_struct->element)
+
+#define S2J_JSON_ARRAY_SET_STRUCT_ELEMENT(array_json, array_struct, type, element ) \
+    S2J_JSON_ARRAY_SET_##type##_STRUCT_ELEMENT(to_json, from_struct, type, element)
+    
 
 #define S2J_CREATE_JSON_OBJECT(json_obj) \
     cJSON *json_obj = cJSON_CreateObject();
@@ -143,6 +190,11 @@ typedef struct {
 
 #define S2J_JSON_SET_BASIC_ELEMENT(to_json, from_struct, type, _element) \
     S2J_JSON_SET_##type##_ELEMENT(to_json, from_struct, _element)
+
+#define S2J_JSON_SET_ENUM_ELEMENT(to_json, from_struct, _element, enum_string) \
+    if (from_struct->_element <= sizeof(enum_string)/sizeof(enum_string[0])) {\
+        cJSON_AddStringToObject(to_json, #_element, enum_string[from_struct->_element]); \
+    }
 
 #define S2J_JSON_SET_ARRAY_ELEMENT(to_json, from_struct, type, _element, size) \
     { \
@@ -162,6 +214,33 @@ typedef struct {
     cJSON *child_json = cJSON_CreateObject(); \
     if (child_json) cJSON_AddItemToObject(to_json, #_element, child_json);
 
+//add struct array to json convert functions.
+#define S2J_JSON_SET_STRUCTARRAY_ELEMENT_START(array_json, to_json, item_json, from_struct, array_struct, type, _element, _level) \
+    { \
+        cJSON *array_json, *item_json;\
+		type *array_struct = from_struct->_element ; \
+        size_t index##_level = 0, size##_level = 0; \
+		size##_level = from_struct->_element ## Len; \
+		if( size##_level > 0) {\
+        	array_json = cJSON_CreateArray(); \
+        	if (array_json) { \
+            	while (index##_level < size##_level) { \
+					index##_level++; \
+					item_json = cJSON_CreateObject(); \
+
+#define S2J_JSON_STRUCTARRAY_SET_ELEMENT(item_json, item_struct, type, _element) \
+					S2J_JSON_ARRAY_SET_STRUCT_ELEMENT( item_json, item_struct, type, _element )
+
+#define S2J_JSON_SET_STRUCTARRAY_ELEMENT_END(array_json, to_json, array_struct, item_json, _element, _level) \
+					array_struct++; \
+					cJSON_AddItemToArray(array_json, item_json); \
+            	} \
+				cJSON_AddItemToObject(to_json, #_element, array_json); \
+        	} \
+		}\
+    }
+
+
 #define S2J_CREATE_STRUCT_OBJECT(struct_obj, type) \
     cJSON *json_temp; \
     type *struct_obj = s2jHook.malloc_fn(sizeof(type)); \
@@ -172,51 +251,64 @@ typedef struct {
 
 #define S2J_STRUCT_GET_BASIC_ELEMENT(to_struct, from_json, type, _element) \
     S2J_STRUCT_GET_##type##_ELEMENT(to_struct, from_json, _element)
+	
+#define S2J_STRUCT_GET_BASIC_ELEMENT_ISUSED(to_struct, from_json, type, _element) \
+    S2J_STRUCT_GET_##type##_ELEMENT_ISUSED(to_struct, from_json, _element)
 
-#define S2J_STRUCT_GET_BASIC_ELEMENT_EX(to_struct, from_json, type, _element, _defval) \
-    S2J_STRUCT_GET_##type##_ELEMENT_EX(to_struct, from_json, _element, _defval)
-
-#define S2J_STRUCT_GET_ARRAY_ELEMENT(to_struct, from_json, type, _element) \
+#define S2J_STRUCT_GET_ARRAY_ELEMENT(to_struct, from_json, type, _element, len) \
     { \
         cJSON *array, *array_element; \
         size_t index = 0, size = 0; \
         array = cJSON_GetObjectItem(from_json, #_element); \
         if (array) { \
             size = cJSON_GetArraySize(array); \
-            while (index < size) { \
+            while ((index < size) && (index < len)) { \
                 array_element = cJSON_GetArrayItem(array, index); \
                 if (array_element) S2J_STRUCT_ARRAY_GET_ELEMENT(to_struct, array_element, type, _element, index++); \
             } \
+            to_struct->_element ## Len = size; \
         } \
     }
 
-#define S2J_STRUCT_GET_ARRAY_ELEMENT_EX(to_struct, from_json, type, _element, size, _defval) \
+
+//add json to struct array convert functions.
+#define S2J_STRUCT_GET_STRUCTARRAY_ELEMENT_START(child_struct, to_struct, from_json, array_json, item_json, type, _element, _level) \
     { \
-        size_t index = 0, realsize = 0; \
-        if (from_json) { \
-            cJSON *array = NULL, *array_element = NULL; \
-            array = cJSON_GetObjectItem(from_json, #_element); \
-            if (array) { \
-                realsize = cJSON_GetArraySize(array); \
-                while (index < realsize) { \
-                    array_element = cJSON_GetArrayItem(array, index); \
-                    S2J_STRUCT_ARRAY_GET_ELEMENT_EX(to_struct, array_element, type, _element, index++, _defval); \
-                } \
-            } else { \
-                while (index < size) { \
-                    S2J_STRUCT_ARRAY_GET_ELEMENT_EX(to_struct, array_element, type, _element, index++, _defval); \
-                } \
+        cJSON *array_json, *item_json; \
+        size_t index##_level = 0, size##_level = 0; \
+		type *child_struct = to_struct->_element ; \
+        array_json = cJSON_GetObjectItem(from_json, #_element); \
+        if (array_json) { \
+            size##_level = cJSON_GetArraySize(array_json); \
+            while (index##_level < size##_level) { \
+                item_json = cJSON_GetArrayItem(array_json, index##_level); \
+                if (item_json) \
+				{ \
+					index##_level++; \
+
+#define S2J_STRUCT_STRUCTARRAY_GET_ELEMENT(child_struct, to_struct, item_json, type, _element) \
+					S2J_STRUCT_ARRAY_GET_STRUCT_ELEMENT(child_struct, to_struct, item_json, type, _element )
+
+#define S2J_STRUCT_GET_STRUCTARRAY_ELEMENT_END(child_struct, to_struct, _element, _level) \
+					child_struct++; \
+				}\
             } \
-        } else { \
-            while (index < size) { \
-                S2J_STRUCT_ARRAY_GET_ELEMENT_EX(to_struct, from_json, type, _element, index++, _defval); \
-            } \
+            to_struct->_element ## Len = size##_level; \
         } \
     }
-	
+
 #define S2J_STRUCT_GET_STRUCT_ELEMENT(child_struct, to_struct, child_json, from_json, type, _element) \
     type *child_struct = &((to_struct)->_element); \
     cJSON *child_json = cJSON_GetObjectItem(from_json, #_element);
+
+#define S2J_STRUCT_GET_STRUCT_ELEMENT_ISUSED_START(child_struct, to_struct, child_json, from_json, type, _element) \
+    type *child_struct = &((to_struct)->_element); \
+    cJSON *child_json = cJSON_GetObjectItem(from_json, #_element); \
+	if( child_json ) { (to_struct)->_element ## _isUsed = 1; 
+
+#define S2J_STRUCT_GET_STRUCT_ELEMENT_ISUSED_END(child_struct, to_struct, child_json, from_json, type, _element) \
+	} \
+	else	(to_struct)->_element ## _isUsed = 0;
 
 #ifdef __cplusplus
 }
